edd17532c2fdd0ce3583e17b7a5d4ab22b173000
jontoshmatov <jontoshmatov@yahoo.com>
2015-12-23 11:13:16 -0500

    games added per Micah

--- database/seeds/GameTableSeeder.php
+++ database/seeds/GameTableSeeder.php
@@ -1,26 +1,29 @@
 <?php
 
-use Illuminate\Database\Seeder;
-use app\Game;
+    use Illuminate\Database\Seeder;
+    use app\Game;
 
-class GameTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    class GameTableSeeder extends Seeder
     {
-        $faker = Faker\Factory::create();
-        DB::table('games')->delete();
-        for ($i = 1; $i <= 5; ++$i) {
-            $games[$i] = Game::create(array(
-                'title' => 'Game Title ' . rand(100, 900),
-                'description' => 'Description: '.$i.$faker->paragraph(1),
-            ));
+        /**
+         * Run the database seeds.
+         *
+         * @return void
+         */
+        public function run()
+        {
+            $faker = Faker\Factory::create();
+            DB::table('games')->delete();
 
-            $this->command->info('Game: "'.$games[$i]->title.'" created!');
+            $games =   "air-transformer, animal-id,be-bright,bike-bits,can-it,cloud-in-a-jar,drought-out,fruit-veg-veg,happy-fish-face,home-scrap-scout,home-sweet-habitat,honey-bee,litter-bug,meerkat-mania,monarch,new-waste-pro,nosey-knows,oyster,plant-pilots,polar-bear,power-bright,real-or-not,safety-first,salad-bowl,school-scrap-scout,sea-turtle,send-a-smile,shower-sluesh,tag-it,tale-of-the-tail,tent-time,tunnel-adventures,twirl-n-swirl,twofer,wally-wall-wall,waste-pro,water-cycle,yawn-spawn";
+            $games = explode(',', $games);
+            foreach($games as $i=>$game) {
+                $title = str_replace('-',' ', $game);
+                $games[] = Game::create(array(
+                    'title' => ucwords($title),
+                    'description' => 'Description: '.$game,
+                ));
+                $this->command->info('Game: "'.$title.'" created!');
+            }
         }
     }
-}
