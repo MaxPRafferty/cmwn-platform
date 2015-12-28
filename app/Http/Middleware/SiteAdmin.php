65e89ff7b801f751dd62e75a1ddd0a05a7db6720
Arron <arron@ginasink.com>
2015-12-28 11:04:12 -0500

    setting up site admin middleware

---
+++ app/Http/Middleware/SiteAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class SiteAdmin
+{
+    public function __construct(Guard $auth)
+    {
+        $this->user = $auth->user();
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$this->user->isSiteAdmin()) {
+            return response('Unauthorized', 401);
+        }
+
+        return $next($request);
+    }
+}
