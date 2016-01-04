e2587de8d66f21a830c7a529539843403be93d35
Arron <arron@ginasink.com>
2016-01-04 13:48:41 -0500

    PSR'ing UserSpecificRepository.php

--- app/cmwn/Users/UserSpecificRepository.php
+++ app/cmwn/Users/UserSpecificRepository.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\cmwn\Users;
+
 use app\Http\Controllers\Api\ApiController;
 use app\Repositories\SideBarItems;
 use app\User;
@@ -8,35 +9,36 @@ use Illuminate\Support\Facades\Auth;
 
 class UserSpecificRepository extends ApiController
 {
-
     public $tag;
     protected $currentUser;
 
-	public function __construct(SideBarItems $tag){
-		$this->tag = $tag;
+    public function __construct(SideBarItems $tag)
+    {
+        $this->tag = $tag;
         $this->currentUser = Auth::user();
-	}
+    }
 
-	public function getApiSideBar(){
-		return $this->tag;
-	}
+    public function getApiSideBar()
+    {
+        return $this->tag;
+    }
 
-	public function compose($view){
-		$acceptedfriends = array();
-		$pendingfriends = array();
-		$friendrequests = array();
-		if (Auth::check()) {
-			$acceptedfriends = User::find($this->currentUser->id)->acceptedfriends;
-			$pendingfriends = User::find($this->currentUser->id)->pendingfriends;
-			$friendrequests = User::find($this->currentUser->id)->friendrequests;
-		}
+    public function compose($view)
+    {
+        $acceptedfriends = array();
+        $pendingfriends = array();
+        $friendrequests = array();
+        if (Auth::check()) {
+            $acceptedfriends = User::find($this->currentUser->id)->acceptedfriends;
+            $pendingfriends = User::find($this->currentUser->id)->pendingfriends;
+            $friendrequests = User::find($this->currentUser->id)->friendrequests;
+        }
 
+        $view->with('tags', $this->tag->getAll())->with('acceptedfriends', $acceptedfriends)->with('pendingfriends', $pendingfriends)->with('friendrequests', $friendrequests);
+    }
 
-
-		$view->with('tags', $this->tag->getAll())->with('acceptedfriends', $acceptedfriends)->with('pendingfriends', $pendingfriends)->with('friendrequests', $friendrequests);
-	}
-
-    public function friendsForApi(){
+    public function friendsForApi()
+    {
         $friends = array();
         if (Auth::check()) {
             $friends['acceptedfriends'] = User::find($this->currentUser->id)->acceptedfriends;
@@ -44,9 +46,6 @@ class UserSpecificRepository extends ApiController
             $friends['friendrequests'] = User::find($this->currentUser->id)->friendrequests;
         }
 
-        return array('data'=>$friends);
+        return array('data' => $friends);
     }
-
-
-
 }
