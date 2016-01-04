09f9ba37709d8da215e48bb7ef27a5cea5c9475c
Arron <arron@ginasink.com>
2016-01-04 13:54:06 -0500

    PSR'ing UserSpecificRepository.php

--- database/seeds/GameTableSeeder.php
+++ database/seeds/GameTableSeeder.php
@@ -7,18 +7,16 @@
     {
         /**
          * Run the database seeds.
-         *
-         * @return void
          */
         public function run()
         {
             $faker = Faker\Factory::create();
             DB::table('games')->delete();
 
-            $games =   "air-transformer, animal-id,be-bright,bike-bits,can-it,cloud-in-a-jar,drought-out,fruit-veg-veg,happy-fish-face,home-scrap-scout,home-sweet-habitat,honey-bee,litter-bug,meerkat-mania,monarch,new-waste-pro,nosey-knows,oyster,plant-pilots,polar-bear,power-bright,real-or-not,safety-first,salad-bowl,school-scrap-scout,sea-turtle,send-a-smile,shower-sluesh,tag-it,tale-of-the-tail,tent-time,tunnel-adventures,twirl-n-swirl,twofer,wally-wall-wall,waste-pro,water-cycle,yawn-spawn";
+            $games = 'air-transformer, animal-id,be-bright,bike-bits,can-it,cloud-in-a-jar,drought-out,fruit-veg-veg,happy-fish-face,home-scrap-scout,home-sweet-habitat,honey-bee,litter-bug,meerkat-mania,monarch,new-waste-pro,nosey-knows,oyster,plant-pilots,polar-bear,power-bright,real-or-not,safety-first,salad-bowl,school-scrap-scout,sea-turtle,send-a-smile,shower-sluesh,tag-it,tale-of-the-tail,tent-time,tunnel-adventures,twirl-n-swirl,twofer,wally-wall-wall,waste-pro,water-cycle,yawn-spawn';
             $games = explode(',', $games);
-            foreach($games as $i=>$game) {
-                $title = str_replace('-',' ', $game);
+            foreach ($games as $i => $game) {
+                $title = str_replace('-', ' ', $game);
                 $games[] = Game::create(array(
                     'title' => ucwords($title),
                     'description' => 'Description: '.$game,
