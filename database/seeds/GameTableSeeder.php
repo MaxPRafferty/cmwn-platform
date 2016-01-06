d1f4a79f07dac47cd409634ee88e5c70f71512db
Adam Walzer <adamwalzer@gmail.com>
2016-01-06 10:59:23 -0500

    game table seeder

--- database/seeds/GameTableSeeder.php
+++ database/seeds/GameTableSeeder.php
@@ -13,15 +13,26 @@
             $faker = Faker\Factory::create();
             DB::table('games')->delete();
 
-            $games = 'air-transformer, animal-id,be-bright,bike-bits,can-it,cloud-in-a-jar,drought-out,fruit-veg-veg,happy-fish-face,home-scrap-scout,home-sweet-habitat,honey-bee,litter-bug,meerkat-mania,monarch,new-waste-pro,nosey-knows,oyster,plant-pilots,polar-bear,power-bright,real-or-not,safety-first,salad-bowl,school-scrap-scout,sea-turtle,send-a-smile,shower-sluesh,tag-it,tale-of-the-tail,tent-time,tunnel-adventures,twirl-n-swirl,twofer,wally-wall-wall,waste-pro,water-cycle,yawn-spawn';
-            $games = explode(',', $games);
-            foreach ($games as $i => $game) {
-                $title = str_replace('-', ' ', $game);
-                $games[] = Game::create(array(
-                    'title' => ucwords($title),
-                    'description' => 'Description: '.$game,
-                ));
-                $this->command->info('Game: "'.$title.'" created!');
-            }
+            $this->command->info('Creating Games!');
+
+            $games[] = User::create(array(
+                'uuid' => 'polar-bear',
+                'title' => 'Polar Bear',
+                'description' => 'Description: Polar Bear description goes here.'
+            ));
+
+            $games[] = User::create(array(
+                'uuid' => 'sea-turtle',
+                'title' => 'Sea Turtle',
+                'description' => 'Description: Sea Turtle description goes here.'
+            ));
+
+            $games[] = User::create(array(
+                'uuid' => 'animal-id',
+                'title' => 'Animal ID',
+                'description' => 'Description: Animal ID description goes here.'
+            ));
+
+            $this->command->info('Games Created!');
         }
     }
