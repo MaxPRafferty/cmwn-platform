875a1e433a81ee29bf9f21097e033023f2d90dc6
Arron <arron@ginasink.com>
2016-01-19 13:55:05 -0500

    more image webhook

--- app/Http/Controllers/Api/ImagesController.php
+++ app/Http/Controllers/Api/ImagesController.php
@@ -4,6 +4,7 @@ namespace app\Http\Controllers\Api;
 
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Request;
+use app\Image;
 
 class ImagesController extends ApiController
 {
@@ -14,10 +15,37 @@ class ImagesController extends ApiController
         $signature = Request::header('X-Cld-Signature');
         $timestamp = Request::header('X-Cld-Timestamp');
         $data = Request::getContent();
-        $api_secert = env('CLOUDINARY_API_KEY');
+        $api_secret = env('CLOUDINARY_API_SECRET');
 
-        if ($signature == sha1("{$data}{$timestamp}{$api_secert}")) {
-            var_dump(Input::get('public_id'));
+        if ($signature == sha1("$data{$timestamp}{$api_secret}")) {
+            if (Input::get('notification_type') == 'moderation') {
+                $image = Image::where('cloudinary_id', Input::get('public_id'))->first();
+
+                if ($image) {
+                    //var_dump(Input::all());
+
+                    $moderation_status = Input::get('moderation_status');
+
+                    if ($moderation_status == 'approved') {
+                        $image->moderation_status = 1;
+                    }
+
+                    if ($moderation_status == 'rejected') {
+                        $image->moderation_status = -1;
+                    }
+
+                    if ($moderation_status == 'pending') {
+                        $image->moderation_status = 0;
+                    }
+
+                    $image->save();
+
+                } else {
+                    return $this->errorNotFound('Failed to locate image.');
+                }
+            } else {
+                return $this->errorNotFound('Invalid Notification Type');
+            }
         } else {
             return $this->errorUnauthorized('Not authorized.');
         }
