4f7a548c8531cac43141bc22fc78f658dae75ae4
Arron <arron@ginasink.com>
2016-01-19 13:09:27 -0500

    adding security to Images webhook

--- app/Http/Controllers/Api/ImagesController.php
+++ app/Http/Controllers/Api/ImagesController.php
@@ -2,9 +2,24 @@
 
 namespace app\Http\Controllers\Api;
 
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Request;
+
 class ImagesController extends ApiController
 {
     public function webhook()
     {
+        //var_dump(Input::all(''));
+
+        $signature = Request::header('X-Cld-Signature');
+        $timestamp = Request::header('X-Cld-Timestamp');
+        $data = Request::getContent();
+        $api_secert = env('CLOUDINARY_API_KEY');
+
+        if ($signature == sha1("{$data}{$timestamp}{$api_secert}")) {
+            var_dump(Input::get('public_id'));
+        } else {
+            return $this->errorUnauthorized('Not authorized.');
+        }
     }
 }
