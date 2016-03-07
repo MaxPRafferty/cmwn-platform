4c35120a3270fa859d48d8475e1d38f0bd1be8b3
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-03-07 17:11:02 -0500

    Updated UserGroupService to find users for a group or organization created endpoints to find all users in groups or orgs

---
+++ module/Api/src/Api/V1/Rest/GroupUsers/GroupUsersCollection.php
@@ -0,0 +1,8 @@
+<?php
+namespace Api\V1\Rest\GroupUsers;
+
+use Zend\Paginator\Paginator;
+
+class GroupUsersCollection extends Paginator
+{
+}
--- module/Group/config/module.config.php
+++ module/Group/config/module.config.php
@@ -3,17 +3,22 @@
 return [
     'service_manager' => [
         'aliases' => [
-            'Group\Service' => 'Group\Service\GroupService'
+            'Group\Service' => 'Group\Service\GroupService',
         ],
         'invokables' => [
-            'Group\Delegator\GroupDelegatorFactory'
+            'Group\Delegator\GroupDelegatorFactory'             => 'Group\Delegator\GroupDelegatorFactory',
+            'Group\Delegator\UserGroupServiceDelegatorFactory'  => 'Group\Delegator\UserGroupServiceDelegatorFactory',
         ],
         'factories' => [
-            'Group\Service\GroupService' => 'Group\Service\GroupServiceFactory'
+            'Group\Service\GroupService'     => 'Group\Service\GroupServiceFactory',
+            'Group\Service\UserGroupService' => 'Group\Service\UserGroupServiceFactory',
         ],
         'delegators' => [
             'Group\Service\GroupService' => [
                 'Group\Delegator\GroupDelegatorFactory'
+            ],
+            'Group\Service\UserGroupService' => [
+                'Group\Delegator\UserGroupServiceDelegatorFactory'
             ],
         ],
     ],
