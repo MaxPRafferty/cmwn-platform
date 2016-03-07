6633a1dbb56a30b5dfa7b4cdb030c82c93e0241e
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-03-07 13:42:53 -0500

    Completed test for Group Service and UserGroup Service

--- module/Group/src/Group/Service/GroupServiceInterface.php
+++ module/Group/src/Group/Service/GroupServiceInterface.php
@@ -4,7 +4,6 @@ namespace Group\Service;
 
 use Application\Exception\NotFoundException;
 use Group\GroupInterface;
-use User\UserInterface;
 use Zend\Db\ResultSet\HydratingResultSet;
 use Zend\Db\Sql\Predicate\PredicateInterface;
 use Zend\Paginator\Adapter\DbSelect;
@@ -16,25 +15,6 @@ use Zend\Paginator\Adapter\DbSelect;
  */
 interface GroupServiceInterface
 {
-    /**
-     * Attaches a user to a group
-     *
-     * @param GroupInterface $group
-     * @param UserInterface $user
-     * @param $role
-     * @return bool
-     */
-    public function attachUserToGroup(GroupInterface $group, UserInterface $user, $role);
-
-    /**
-     * Detaches a user from a group
-     *
-     * @param GroupInterface $group
-     * @param UserInterface $user
-     * @return bool
-     */
-    public function detachUserFromGroup(GroupInterface $group, UserInterface $user);
-
     /**
      * @param GroupInterface $parent
      * @param GroupInterface $child
