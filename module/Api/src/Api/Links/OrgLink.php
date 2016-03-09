a5a0722221a3096cab4a5ff10f721c07e0764555
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-03-09 17:02:34 -0500

    Finished Hal Links for Me entity

--- module/Api/src/Api/Links/OrgLink.php
+++ module/Api/src/Api/Links/OrgLink.php
@@ -14,7 +14,7 @@ class OrgLink extends Link
     public function __construct(OrganizationInterface $org, $scope = null)
     {
         parent::__construct(strtolower('org_' . $org->getType()));
-        $this->setRoute('api.rest.org', ['type' => $org->getType()]);
+        $this->setRoute('api.rest.org', [], ['query' => ['type' => $org->getType()]]);
 
         // TODO get the label for this type
         $props = ['label' => 'My ' . $org->getType()];
--- module/Security/src/Security/Service/SecurityOrgService.php
+++ module/Security/src/Security/Service/SecurityOrgService.php
@@ -6,6 +6,7 @@ use Org\Organization;
 use Security\SecurityUser;
 use Zend\Db\Adapter\Adapter;
 use Zend\Db\Sql\Expression;
+use Zend\Db\Sql\Predicate\In;
 use Zend\Db\Sql\Predicate\Operator;
 use Zend\Db\Sql\Select;
 use Zend\Db\Sql\Sql;
@@ -33,6 +34,7 @@ class SecurityOrgService
     }
 
     /**
+     * Attaches the 1st 10 organizations to a logged in user
      *
      * SELECT
      *   DISTINCT(o.org_id) AS org_id,
@@ -46,11 +48,13 @@ class SecurityOrgService
      * LIMIT 10;
      *
      * @param SecurityUser $user
+     * @return $this
+     * @todo Cache
      */
-    public function attachOrganizationsToUser(SecurityUser &$user)
+    public function attachOrganizationsToUser(SecurityUser $user)
     {
         if ($user->isSuper()) {
-            return;
+            return $this;
         }
 
         $select = new Select();
@@ -78,5 +82,52 @@ class SecurityOrgService
         foreach ($orgs as $orgData) {
             $user->addOrganization(new Organization($orgData));
         }
+
+        return $this;
+    }
+
+    /**
+     * Attaches the group types to a logged in user
+     *
+     * SELECT
+     * DISTINCT g.type
+     * FROM groups g
+     *   LEFT JOIN user_groups ug ON ug.group_id = g.group_id
+     * WHERE ug.user_id = 'b4e9147a-e60a-11e5-b8ea-0800274f2cef'
+     *   AND g.organization_id IN ('6bb4900e-e605-11e5-8c29-0800274f2cef', '6bb41c82-e605-11e5-a7a7-0800274f2cef');
+     *
+     * @param SecurityUser $user
+     * @return $this
+     * @todo Cache
+     */
+    public function attachGroupTypesToUser(SecurityUser $user)
+    {
+        if ($user->isSuper()) {
+            return $this;
+        }
+
+        $orgIds = array_keys($user->getOrganizations());
+        $select = new Select();
+        $select->columns([
+            new Expression('DISTINCT(g.type) AS type'),
+        ]);
+
+        $where = new Where();
+        $where->addPredicate(new In('organization_id', $orgIds));
+
+        $select->from(['g'  => 'groups']);
+        $select->join(['ug' => 'user_groups'], 'ug.group_id = g.group_id', [], Select::JOIN_LEFT);
+        $select->where($where);
+        $select->limit(10);
+
+        $sql   = new Sql($this->adapter);
+        $stmt  = $sql->prepareStatementForSqlObject($select);
+        $types = $stmt->execute();
+
+        foreach ($types as $groupType) {
+            $user->addGroupType($groupType['type']);
+        }
+
+        return $this;
     }
 }
