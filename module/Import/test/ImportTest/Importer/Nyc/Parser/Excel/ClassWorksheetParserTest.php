00f93789c3820a3759d8cda41b058297a1a71bbb
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-03-11 10:04:21 -0500

    Tied Registry into parser

--- module/Import/test/ImportTest/Importer/Nyc/Parser/Excel/ClassWorksheetParserTest.php
+++ module/Import/test/ImportTest/Importer/Nyc/Parser/Excel/ClassWorksheetParserTest.php
@@ -2,6 +2,9 @@
 
 namespace ImportTest\Importer\Nyc\Parser\Excel;
 
+use Import\Importer\Nyc\ClassRoom\ClassRoom;
+use Import\Importer\Nyc\ClassRoom\ClassRoomRegistry;
+use Import\Importer\Nyc\Exception\InvalidWorksheetException;
 use Import\Importer\Nyc\Parser\Excel\ClassWorksheetParser;
 use \PHPUnit_Framework_TestCase as TestCase;
 
@@ -10,11 +13,121 @@ use \PHPUnit_Framework_TestCase as TestCase;
  */
 class ClassWorksheetParserTest extends TestCase
 {
+    /**
+     * @var \Mockery\MockInterface|\Group\Service\GroupService
+     */
+    protected $groupService;
+
+    /**
+     * @var ClassRoomRegistry
+     */
+    protected $registry;
+
+    /**
+     * @before
+     */
+    public function setUpGroupService()
+    {
+        $this->groupService = \Mockery::mock('\Group\Service\GroupService');
+        $this->groupService->shouldNotReceive('fetchGroup');
+    }
+
+    /**
+     * @before
+     */
+    public function setUpRegistry()
+    {
+        $this->registry = new ClassRoomRegistry($this->groupService);
+    }
+
+    /**
+     * @param \PHPExcel_Worksheet $sheet
+     * @return ClassWorksheetParser
+     */
+    protected function getParser(\PHPExcel_Worksheet $sheet)
+    {
+        return new ClassWorksheetParser($sheet, $this->registry);
+    }
+
+    /**
+     * @return ClassRoom[]
+     */
+    protected function getExpectedClassrooms()
+    {
+        return [
+            '001'  => new ClassRoom('Lunch', '001', ['8001', '8002']),
+            '102'  => new ClassRoom('PreK', '102', ['8001', '8002']),
+            '011'  => new ClassRoom('Kindergarten', '011', ['8001', '8002']),
+            '101'  => new ClassRoom('First Grade', '101', ['8001', '8002', '8003']),
+            '201'  => new ClassRoom('Second Grade', '201', ['8001', '8002', '8003']),
+            '301'  => new ClassRoom('Third Grade', '301', ['8001', '8002', '8003']),
+            '8001' => new ClassRoom('Physical Education', '8001', []),
+            '8002' => new ClassRoom('Art', '8002', []),
+            '8003' => new ClassRoom('Technology', '8003', []),
+        ];
+    }
+
+    protected function checkRegistry()
+    {
+        foreach ($this->getExpectedClassrooms() as $classId => $classroom) {
+            $this->assertTrue(
+                $this->registry->offsetExists($classId),
+                sprintf('Parser did not add class room "%s" to registry', $classId)
+            );
+
+            $this->assertEquals(
+                $classroom,
+                $this->registry->offsetGet($classId),
+                sprintf('Parser created invalid class room for class "%s"', $classId)
+            );
+        }
+    }
+
+    public function testItShouldItShouldStoreClassesInTheRegistry()
+    {
+        $reader = \PHPExcel_IOFactory::load(__DIR__ . '/_files/class_good_sheet.xlsx');
+        $sheet  = $reader->getSheet(0);
+        $parser = $this->getParser($sheet);
+        $parser->preProcess();
+
+        $this->assertFalse($parser->hasErrors(), 'Parser Reported errors for a good classes sheet');
+        $this->assertEmpty($parser->getErrors(), 'Parser has errors reported for a good classes sheet');
+        $this->assertFalse($parser->hasWarnings(), 'Parser Reported warnings for a good classes sheet');
+        $this->assertEmpty($parser->getWarnings(), 'Parser has warnings reported for a good classes sheet');
+
+        $this->checkRegistry();
+    }
+
+    public function testItShouldWarnOnEmptyLine()
+    {
+        $reader = \PHPExcel_IOFactory::load(__DIR__ . '/_files/class_good_sheet_with_empty_line.xlsx');
+        $sheet  = $reader->getSheet(0);
+        $parser = $this->getParser($sheet);
+        $parser->preProcess();
+
+        $expectedWarnings = [
+            'Sheet "Classes" Row: 4 No data found between cells "A" and "D" Skipping this row',
+            'Sheet "Classes" Row: 7 No data found between cells "A" and "D" Skipping this row',
+            'Sheet "Classes" Row: 9 No data found between cells "A" and "D" Skipping this row',
+        ];
+
+        $this->assertFalse($parser->hasErrors(), 'Parser Reported errors for a good classes sheet');
+        $this->assertEmpty($parser->getErrors(), 'Parser has errors reported for a good classes sheet');
+        $this->assertTrue($parser->hasWarnings(), 'Parser did not Report warnings for a sheet with an empty');
+        $this->assertSame(
+            $expectedWarnings,
+            $parser->getWarnings(),
+            'Parser reported incorrect warnings for sheet with empty line'
+        );
+
+        $this->checkRegistry();
+    }
+
     public function testItShouldErrorWithInvalidDdbnnn()
     {
         $reader = \PHPExcel_IOFactory::load(__DIR__ . '/_files/class_invalid_ddbnnn.xlsx');
         $sheet  = $reader->getSheet(0);
-        $parser = new ClassWorksheetParser($sheet);
+        $parser = $this->getParser($sheet);
         $parser->preProcess();
 
         $expectedErrors = ['Sheet "Classes" Row: 2 Invalid DDBNNN 144Q1001'];
@@ -46,7 +159,7 @@ class ClassWorksheetParserTest extends TestCase
     {
         $reader = \PHPExcel_IOFactory::load(__DIR__ . '/_files/class_missing_title.xlsx');
         $sheet  = $reader->getSheet(0);
-        $parser = new ClassWorksheetParser($sheet);
+        $parser = $this->getParser($sheet);
         $parser->preProcess();
 
         $expectedErrors = ['Sheet "Classes" Row: 2 Missing class title'];
@@ -78,7 +191,7 @@ class ClassWorksheetParserTest extends TestCase
     {
         $reader = \PHPExcel_IOFactory::load(__DIR__ . '/_files/class_missing_class_id.xlsx');
         $sheet  = $reader->getSheet(0);
-        $parser = new ClassWorksheetParser($sheet);
+        $parser = $this->getParser($sheet);
         $parser->preProcess();
 
         $expectedErrors = ['Sheet "Classes" Row: 2 Missing class id'];
@@ -105,4 +218,88 @@ class ClassWorksheetParserTest extends TestCase
             'Parser reported warnings that It should not have'
         );
     }
+
+    public function testItShouldErrorWhenMissingSheet()
+    {
+        $this->setExpectedException(
+            InvalidWorksheetException::class,
+            'Missing worksheet "Classes"'
+        );
+
+        $this->getParser(new \PHPExcel_Worksheet(new \PHPExcel()));
+    }
+
+    public function testItShouldErrorWithEmptyWorksheet()
+    {
+        $excel = new \PHPExcel();
+        $sheet = $excel->getActiveSheet();
+        $sheet->setTitle('Classes');
+        $parser = $this->getParser($sheet);
+        $parser->preProcess();
+
+        $expectedErrors = [
+            'Sheet "Classes" Row: 1 Column "A" in the header is not labeled as "DDBNNN"',
+            'Sheet "Classes" Row: 1 Is missing one or more column(s) between "A" and "D"',
+        ];
+
+        $this->assertTrue(
+            $parser->hasErrors(),
+            'Parser did not produce any errors when a class is missing an id'
+        );
+
+        $this->assertSame(
+            $expectedErrors,
+            $parser->getErrors(),
+            'Parser did not report correct error messages when a class was missing an id'
+        );
+
+        $this->assertFalse(
+            $parser->hasWarnings(),
+            'Parser is reporting warnings when this file should not have any'
+        );
+
+        $this->assertSame(
+            [],
+            $parser->getWarnings(),
+            'Parser reported warnings that It should not have'
+        );
+    }
+
+    public function testItShouldReportErrorWithBadHeadeHeaderLables()
+    {
+        $reader = \PHPExcel_IOFactory::load(__DIR__ . '/_files/class_bad_header.xlsx');
+        $sheet  = $reader->getSheet(0);
+        $parser = $this->getParser($sheet);
+        $parser->preProcess();
+
+        $expectedErrors = [
+            'Sheet "Classes" Row: 1 Column "A" in the header is not labeled as "DDBNNN"',
+            'Sheet "Classes" Row: 1 Column "B" in the header is not labeled as "TITLE"',
+            'Sheet "Classes" Row: 1 Column "C" in the header is not labeled as "OFF CLS"',
+            'Sheet "Classes" Row: 1 Column "D" in the header is not labeled as "SUB CLASSES"',
+        ];
+
+        $this->assertTrue(
+            $parser->hasErrors(),
+            'Parser did not produce any errors when a class is missing an id'
+        );
+
+        $this->assertSame(
+            $expectedErrors,
+            $parser->getErrors(),
+            'Parser did not report correct error messages when a class was missing an id'
+        );
+
+        $this->assertFalse(
+            $parser->hasWarnings(),
+            'Parser is reporting warnings when this file should not have any'
+        );
+
+        $this->assertSame(
+            [],
+            $parser->getWarnings(),
+            'Parser reported warnings that It should not have'
+        );
+    }
+
 }
