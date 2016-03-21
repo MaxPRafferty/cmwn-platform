4a04b9d774ad9ae37b72b90901d701376ddeeca6
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-03-21 16:15:22 -0400

    Added more allowed token routes

--- module/Api/src/Api/Listeners/CsrfListener.php
+++ module/Api/src/Api/Listeners/CsrfListener.php
@@ -62,7 +62,7 @@ class CsrfListener extends Csrf
      */
     public function checkToken(ResourceEvent $event)
     {
-        if ($event->getRouteMatch()->getMatchedRouteName() === 'api.rest.token') {
+        if (in_array(['api.rest.token', 'api.rest.logout', 'api.rest.forgot'], $event->getRouteMatch()->getMatchedRouteName())) {
             return null;
         }
 
