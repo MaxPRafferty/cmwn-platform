b679ccff01510ddc0a41b1250402a7614eed2030
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-03-22 11:55:04 -0400

    User Entity always returns the games link

--- module/Api/src/Api/V1/Rest/User/UserEntity.php
+++ module/Api/src/Api/V1/Rest/User/UserEntity.php
@@ -2,17 +2,39 @@
 
 namespace Api\V1\Rest\User;
 
+use Api\Links\GameLink;
 use User\User;
 use User\UserInterface;
+use ZF\Hal\Link\LinkCollection;
+use ZF\Hal\Link\LinkCollectionAwareInterface;
 
 /**
  * Class UserEntity
  *
  * @package Api\V1\Rest\User
  */
-class UserEntity extends User implements UserInterface
+class UserEntity extends User implements UserInterface, LinkCollectionAwareInterface
 {
+    /**
+     * @var string
+     */
     protected $type;
+
+    /**
+     * @var LinkCollection
+     */
+    protected $links;
+
+    /**
+     * @return mixed
+     */
+    public function getArrayCopy()
+    {
+        return array_merge(
+            parent::getArrayCopy(),
+            ['links' => $this->getLinks()]
+        );
+    }
 
     /**
      * @param $type
@@ -28,5 +50,32 @@ class UserEntity extends User implements UserInterface
     public function getType()
     {
         return $this->type;
+    }
+
+    /**
+     * Set link collection
+     *
+     * @param  LinkCollection $links
+     * @return self
+     */
+    public function setLinks(LinkCollection $links)
+    {
+        $this->links = $links;
+        $this->links->add(new GameLink());
+        return $this;
+    }
+
+    /**
+     * Get link collection
+     *
+     * @return LinkCollection
+     */
+    public function getLinks()
+    {
+        if (!$this->links instanceof LinkCollection) {
+            $this->setLinks(new LinkCollection());
+        }
+
+        return $this->links;
     }
 }
