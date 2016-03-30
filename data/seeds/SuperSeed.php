309606763c1f7e1e71178aa6d8b95021e20c752f
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-03-30 14:23:47 -0400

    Updated super seed for everyone in the company

--- data/seeds/SuperSeed.php
+++ data/seeds/SuperSeed.php
@@ -36,7 +36,7 @@ class SuperSeed extends AbstractSeed
             ])
                 ->save();
         } catch (PDOException $exception) {
-            $this->getOutput()->write(
+            $this->getOutput()->writeln(
                 'Got Exception When creating max (this might be ok): ' . $exception->getMessage()
             );
         }
@@ -60,7 +60,7 @@ class SuperSeed extends AbstractSeed
             ])
                 ->save();
         } catch (PDOException $exception) {
-            $this->getOutput()->write(
+            $this->getOutput()->writeln(
                 'Got Exception When creating joni (this might be ok): ' . $exception->getMessage()
             );
         }
@@ -84,8 +84,200 @@ class SuperSeed extends AbstractSeed
             ])
                 ->save();
         } catch (PDOException $exception) {
-            $this->getOutput()->write(
+            $this->getOutput()->writeln(
                 'Got Exception When creating chuck (this might be ok): ' . $exception->getMessage()
+            );
+        }
+
+        try {
+            $table->insert([
+                'user_id'    => \Ramsey\Uuid\Uuid::uuid1(),
+                'username'   => 'csinoradzki',
+                'email'      => 'cathy@ginasink.com',
+                'code'       => null,
+                'type'       => 'ADULT',
+                'password'   => password_hash('business', PASSWORD_DEFAULT),
+                'first_name' => 'Cathy',
+                'last_name'  => 'Snooteriski',
+                'gender'     => 'female',
+                'meta'       => '[]',
+                'birthdate'  => null,
+                'created'    => $date->format("Y-m-d H:i:s"),
+                'updated'    => $date->format("Y-m-d H:i:s"),
+                'super'      => 1
+            ])
+                ->save();
+        } catch (PDOException $exception) {
+            $this->getOutput()->writeln(
+                'Got Exception When creating cathy (this might be ok): ' . $exception->getMessage()
+            );
+        }
+
+        try {
+            $table->insert([
+                'user_id'    => \Ramsey\Uuid\Uuid::uuid1(),
+                'username'   => 'jfontaina',
+                'email'      => 'jasmine@ginasink.com',
+                'code'       => null,
+                'type'       => 'ADULT',
+                'password'   => password_hash('business', PASSWORD_DEFAULT),
+                'first_name' => 'Jasmine',
+                'last_name'  => 'Fontaina',
+                'gender'     => 'female',
+                'meta'       => '[]',
+                'birthdate'  => null,
+                'created'    => $date->format("Y-m-d H:i:s"),
+                'updated'    => $date->format("Y-m-d H:i:s"),
+                'super'      => 1
+            ])
+                ->save();
+        } catch (PDOException $exception) {
+            $this->getOutput()->writeln(
+                'Got Exception When creating jasmine (this might be ok): ' . $exception->getMessage()
+            );
+        }
+
+        try {
+            $table->insert([
+                'user_id'    => \Ramsey\Uuid\Uuid::uuid1(),
+                'username'   => 'awalzer',
+                'email'      => 'adam@ginasink.com',
+                'code'       => null,
+                'type'       => 'ADULT',
+                'password'   => password_hash('business', PASSWORD_DEFAULT),
+                'first_name' => 'Adam',
+                'last_name'  => 'Walzer',
+                'gender'     => 'male',
+                'meta'       => '[]',
+                'birthdate'  => null,
+                'created'    => $date->format("Y-m-d H:i:s"),
+                'updated'    => $date->format("Y-m-d H:i:s"),
+                'super'      => 1
+            ])
+                ->save();
+        } catch (PDOException $exception) {
+            $this->getOutput()->writeln(
+                'Got Exception When creating adam (this might be ok): ' . $exception->getMessage()
+            );
+        }
+
+        try {
+            $table->insert([
+                'user_id'    => \Ramsey\Uuid\Uuid::uuid1(),
+                'username'   => 'mrolon',
+                'email'      => 'micah@ginasink.com',
+                'code'       => null,
+                'type'       => 'ADULT',
+                'password'   => password_hash('business', PASSWORD_DEFAULT),
+                'first_name' => 'Micah',
+                'last_name'  => 'Rolon',
+                'gender'     => 'male',
+                'meta'       => '[]',
+                'birthdate'  => null,
+                'created'    => $date->format("Y-m-d H:i:s"),
+                'updated'    => $date->format("Y-m-d H:i:s"),
+                'super'      => 1
+            ])
+                ->save();
+        } catch (PDOException $exception) {
+            $this->getOutput()->writeln(
+                'Got Exception When creating micha (this might be ok): ' . $exception->getMessage()
+            );
+        }
+
+        try {
+            $table->insert([
+                'user_id'    => \Ramsey\Uuid\Uuid::uuid1(),
+                'username'   => 'gotto',
+                'email'      => 'gina@ginasink.com',
+                'code'       => null,
+                'type'       => 'ADULT',
+                'password'   => password_hash('business', PASSWORD_DEFAULT),
+                'first_name' => 'Gina',
+                'last_name'  => 'Otto',
+                'gender'     => 'female',
+                'meta'       => '[]',
+                'birthdate'  => null,
+                'created'    => $date->format("Y-m-d H:i:s"),
+                'updated'    => $date->format("Y-m-d H:i:s"),
+                'super'      => 1
+            ])
+                ->save();
+        } catch (PDOException $exception) {
+            $this->getOutput()->writeln(
+                'Got Exception When creating gina (this might be ok): ' . $exception->getMessage()
+            );
+        }
+
+        try {
+            $table->insert([
+                'user_id'    => \Ramsey\Uuid\Uuid::uuid1(),
+                'username'   => 'vdorazio',
+                'email'      => 'valerie@ginasink.com',
+                'code'       => null,
+                'type'       => 'ADULT',
+                'password'   => password_hash('business', PASSWORD_DEFAULT),
+                'first_name' => 'Valerie',
+                'last_name'  => 'D\'Orazio',
+                'gender'     => 'female',
+                'meta'       => '[]',
+                'birthdate'  => null,
+                'created'    => $date->format("Y-m-d H:i:s"),
+                'updated'    => $date->format("Y-m-d H:i:s"),
+                'super'      => 1
+            ])
+                ->save();
+        } catch (PDOException $exception) {
+            $this->getOutput()->writeln(
+                'Got Exception When creating valerie (this might be ok): ' . $exception->getMessage()
+            );
+        }
+
+        try {
+            $table->insert([
+                'user_id'    => \Ramsey\Uuid\Uuid::uuid1(),
+                'username'   => 'mwren',
+                'email'      => 'marilyn@ginasink.com',
+                'code'       => null,
+                'type'       => 'ADULT',
+                'password'   => password_hash('business', PASSWORD_DEFAULT),
+                'first_name' => 'Marilyn',
+                'last_name'  => 'Wren',
+                'gender'     => 'female',
+                'meta'       => '[]',
+                'birthdate'  => null,
+                'created'    => $date->format("Y-m-d H:i:s"),
+                'updated'    => $date->format("Y-m-d H:i:s"),
+                'super'      => 1
+            ])
+                ->save();
+        } catch (PDOException $exception) {
+            $this->getOutput()->writeln(
+                'Got Exception When creating Marilyn (this might be ok): ' . $exception->getMessage()
+            );
+        }
+
+        try {
+            $table->insert([
+                'user_id'    => \Ramsey\Uuid\Uuid::uuid1(),
+                'username'   => 'emclean',
+                'email'      => 'emily@ginasink.com',
+                'code'       => null,
+                'type'       => 'ADULT',
+                'password'   => password_hash('business', PASSWORD_DEFAULT),
+                'first_name' => 'Emily',
+                'last_name'  => 'Mclean',
+                'gender'     => 'female',
+                'meta'       => '[]',
+                'birthdate'  => null,
+                'created'    => $date->format("Y-m-d H:i:s"),
+                'updated'    => $date->format("Y-m-d H:i:s"),
+                'super'      => 1
+            ])
+                ->save();
+        } catch (PDOException $exception) {
+            $this->getOutput()->writeln(
+                'Got Exception When creating Marilyn (this might be ok): ' . $exception->getMessage()
             );
         }
     }
