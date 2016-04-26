7fc0eb60ad2cef5383430b3cfe932c284dcc3408
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-04-26 16:20:41 -0400

    Added Flip HAL Links CORE-233

---
+++ module/Api/src/Api/Links/UserFlipLink.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Api\Links;
+
+use ZF\Hal\Link\Link;
+
+/**
+ * Class UserFlipLink
+ */
+class UserFlipLink extends Link
+{
+    /**
+     * UserFlipLink constructor.
+     *
+     * @param null|string $userId
+     */
+    public function __construct($userId = null)
+    {
+        parent::__construct('user_flips');
+        $this->setRoute('api.rest.flip-user', ['user_id' => $userId]);
+    }
+}
