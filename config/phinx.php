9880cbce54b8b398e2e32b0f0c8c6bd4af668391
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-05-11 12:03:02 -0400

    Wercker fixes

--- config/phinx.php
+++ config/phinx.php
@@ -6,10 +6,9 @@ $dbHost = getenv('DATABASE1_HOST');
 $dbUser = getenv('DATABASE1_USER');
 $dbPass = getenv('DATABASE1_PASS');
 
-$testName = getenv('MYSQL_SERVER_ENV_MYSQL_DATABASE') === false ? 'cmwn_test' : getenv('MYSQL_SERVER_ENV_MYSQL_DATABASE');
-$testHost = getenv('MYSQL_SERVER_PORT_3306_TCP_ADDR') === false ? 'localhost' : getenv('MYSQL_SERVER_PORT_3306_TCP_ADDR');
-$testUser = getenv('MYSQL_SERVER_ENV_MYSQL_ROOT_USERNAME') === false ? 'cmwn_user' : getenv('MYSQL_SERVER_ENV_MYSQL_ROOT_USERNAME');
-$testPass = getenv('MYSQL_SERVER_ENV_MYSQL_ROOT_PASSWORD') === false ? 'cmwn_pass123$' : getenv('MYSQL_SERVER_ENV_MYSQL_ROOT_PASSWORD');
+$testHost = getenv('MYSQL_PORT_3306_TCP_ADDR') === false ? 'localhost' : getenv('MYSQL_PORT_3306_TCP_ADDR');
+$testUser = getenv('MYSQL_ENV_MYSQL_USER') === false ? 'cmwn_user' : getenv('MYSQL_ENV_MYSQL_USER');
+$testPass = getenv('MYSQL_ENV_MYSQL_PASSWORD') === false ? 'cmwn_pass123$' : getenv('MYSQL_ENV_MYSQL_PASSWORD');
 
 $config = [
     'paths'        => [
@@ -36,7 +35,7 @@ $config = [
         'test'                    => [
             'adapter' => 'mysql',
             'host'    => $testHost,
-            'name'    => $testName,
+            'name'    => 'cmwn_test',
             'user'    => $testUser,
             'pass'    => $testPass,
         ],
