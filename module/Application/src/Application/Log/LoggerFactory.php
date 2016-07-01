1012707d095c62cf6902caff9f97b339fa267388
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-07-01 15:46:06 -0400

    Adding Logging to file since rollbar is cutting off my trace

--- module/Application/src/Application/Log/LoggerFactory.php
+++ module/Application/src/Application/Log/LoggerFactory.php
@@ -6,6 +6,7 @@ use Application\Log\Rollbar\Writer;
 use Interop\Container\ContainerInterface;
 use Zend\Log\LoggerAbstractServiceFactory;
 use Zend\Log\Writer\Noop;
+use Zend\Log\Writer\Stream;
 
 /**
  * Class LoggerFactory
@@ -23,6 +24,8 @@ class LoggerFactory extends LoggerAbstractServiceFactory
         // TODO get the writer from a config
         $writer = (defined('TEST_MODE') && TEST_MODE) ? new Noop() : $container->get(Writer::class);
         $logger->addWriter($writer);
+        $file = realpath(__DIR__ . '/../../../../../data/logs/') . '/app.log';
+        $logger->addWriter(new Stream($file));
         return $logger;
     }
 }
