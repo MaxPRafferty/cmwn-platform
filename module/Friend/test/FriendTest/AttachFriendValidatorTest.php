314e8d5b4a03b53d578ba306bd64cfbfb307439c
chaithra-yenikapati <chaithrayenikapati@gmail.com>
2016-07-13 15:32:49 -0400

    added file

---
+++ module/Friend/test/FriendTest/AttachFriendValidatorTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace FriendTest;
+
+use \PHPUnit_Framework_TestCase as TestCase;
+
+/**
+ * Test AttachFriendValidatorTest
+ * @package FriendTest
+ * @group Friend
+ * @group Validator
+ */
+class AttachFriendValidatorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testItShouldValidate()
+    {
+       
+    }
+}
