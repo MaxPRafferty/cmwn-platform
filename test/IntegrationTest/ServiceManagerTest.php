57143384d95c0a76ad328c7db30c58a657c9e09d
chaithra-yenikapati <chaithrayenikapati@gmail.com>
2016-07-27 14:39:35 -0400

    Fixing tests

--- test/IntegrationTest/ServiceManagerTest.php
+++ test/IntegrationTest/ServiceManagerTest.php
@@ -31,7 +31,8 @@ class ServiceManagerTest extends TestCase
         'ZF\OAuth2\Adapter\MongoAdapter',
         'Zend\Session\SessionManager',
         'AwsModule\Session\SaveHandler\DynamoDb',
-        'Log\App'
+        'Log\App',
+        'ZF\Configuration\ConfigResource'
     ];
 
     /**
@@ -98,5 +99,4 @@ class ServiceManagerTest extends TestCase
 
         $this->assertTrue(true);
     }
-
 }
