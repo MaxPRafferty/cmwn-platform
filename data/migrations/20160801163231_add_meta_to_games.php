921f0932545f3a9bdb3e56cb7968ca478e42671a
chaithra-yenikapati <chaithrayenikapati@gmail.com>
2016-08-01 14:46:25 -0400

    updating games to have meta field and adding all-about-you to games.

---
+++ data/migrations/20160801163231_add_meta_to_games.php
@@ -0,0 +1,43 @@
+<?php
+// @codingStandardsIgnoreStart
+use Phinx\Migration\AbstractMigration;
+
+class AddMetaToGames extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('games', ['id' => false, 'primary_key' => ['game_id']]);
+
+        if ($table->hasColumn('meta')) {
+            $table->removeColumn('meta');
+        }
+
+        if (!$table->hasColumn('meta')) {
+            $table->addColumn('meta', 'text', ['null' => true]);
+        }
+
+        $table->update();
+        return null;
+    }
+}
