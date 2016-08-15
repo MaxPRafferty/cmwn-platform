8c8e170bd1d0f3acf75e3200fb8a8dd72b6db989
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-08-15 14:32:58 -0400

    Updated test since it was wrong

--- test/IntegrationTest/Api/V1/Rest/UserResourceTest.php
+++ test/IntegrationTest/Api/V1/Rest/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace IntegrationTest\Api\V1\Rest;
 
+use Application\Exception\NotFoundException;
 use IntegrationTest\AbstractApigilityTestCase as TestCase;
 use IntegrationTest\TestHelper;
 use User\Service\UserServiceInterface;
@@ -402,15 +403,18 @@ class UserResourceTest extends TestCase
      * @test
      * @ticket CORE-727
      */
-    public function testItShouldNotLetTeacherDeleteStudentProfile()
+    public function testItShouldLetTeacherDeleteStudentProfile()
     {
         $this->injectValidCsrfToken();
         $this->logInUser('english_teacher');
         $this->dispatch('/user/english_student', 'DELETE');
 
-        $this->assertResponseStatusCode(403);
+        $this->assertResponseStatusCode(200);
         $this->assertMatchedRouteName('api.rest.user');
         $this->assertControllerName('api\v1\rest\user\controller');
+
+        $this->setExpectedException(NotFoundException::class);
+        $this->loadUserFromDb('english_student');
     }
 
     /**
