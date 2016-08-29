12cbbc75f9884b8faa804b547aef97184aa7f120
chaithra-yenikapati <chaithrayenikapati@gmail.com>
2016-08-29 11:54:29 -0400

    eliminating complications

--- module/Api/src/Api/V1/Rest/Feed/FeedResource.php
+++ module/Api/src/Api/V1/Rest/Feed/FeedResource.php
@@ -2,11 +2,8 @@
 
 namespace Api\V1\Rest\Feed;
 
-use Api\Links\GameLink;
 use Api\V1\Rest\Game\GameResource;
-use Game\Game;
 use Game\Service\GameService;
-use Zend\Paginator\Adapter\ArrayAdapter;
 use Zend\Paginator\Adapter\DbSelect;
 use ZF\Rest\AbstractResourceListener;
 
@@ -30,41 +27,14 @@ class FeedResource extends AbstractResourceListener
     }
 
     /**
-     * @param $games
-     * @return array
-     */
-    protected function createFeedFromGames($games)
-    {
-        $feeds = [];
-        /**@var \Game\GameInterface $game*/
-        foreach ($games as $game) {
-            $feedData = [
-                'type' => 'game',
-            ];
-
-            $gameData = $game->getArrayCopy();
-            $gameId = $game->getGameId();
-            $feedData['header'] = $game->getTitle();
-            $feedData['message'] = $game->getDescription();
-            $feedData['created'] = isset($gameData['created']) ? $gameData['created'] : null;
-            $feedData['link'] = new GameLink($gameId);
-            $feeds[] = new FeedEntity($feedData);
-        }
-        return $feeds;
-    }
-
-    /**
      * @param array $params
      * @return FeedCollection
      */
     public function fetchAll($params = [])
     {
         /**@var DbSelect $games*/
-        $games = $this->gameService->fetchAll(null, true, new Game());
+        $feeds = $this->gameService->fetchAll(null, true, new FeedEntity());
 
-        $games = $games->getItems(0, count($games));
-        $feeds = $this->createFeedFromGames($games);
-
-        return new FeedCollection(new ArrayAdapter($feeds));
+        return new FeedCollection($feeds);
     }
 }
