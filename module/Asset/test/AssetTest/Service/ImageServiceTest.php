dcd9f741a239436c8a42c58281952a09ae739087
chaithra-yenikapati <chaithrayenikapati@gmail.com>
2016-08-29 10:12:55 -0400

    Fixing broken profile images

--- module/Asset/test/AssetTest/Service/ImageServiceTest.php
+++ module/Asset/test/AssetTest/Service/ImageServiceTest.php
@@ -145,7 +145,7 @@ class ImageServiceTest extends TestCase
         ];
 
         $image   = new Image($imageData);
-        $image->setModerationStatus(1);
+        $image->setModerationStatus(-1);
         $result = new ResultSet();
         $result->initialize([$imageData]);
         $this->tableGateway->shouldReceive('select')
