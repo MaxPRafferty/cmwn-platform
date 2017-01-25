90bdd0064f78b9fa4189d5d65d90e7410b23901a
chaithra-yenikapati <chaithrayenikapati@gmail.com>
2017-01-25 12:19:17 -0500

    hal links tests

---
+++ module/Group/test/GroupTest/Rule/Provider/ChildGroupTypesProviderTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace GroupTest\Rule\Provider;
+
+use Api\V1\Rest\Group\GroupEntity;
+use Api\V1\Rest\User\UserEntity;
+use Group\Rule\Provider\ChildGroupTypesProvider;
+use Group\Service\GroupServiceInterface;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Zend\EventManager\Event;
+use ZF\Hal\Entity;
+
+/**
+ * Class ChildGroupTypesProviderTest
+ * @package GroupTest\Rule\Provider
+ */
+class ChildGroupTypesProviderTest extends \PHPUnit_Framework_TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var \Mockery\MockInterface | GroupServiceInterface
+     */
+    protected $groupService;
+
+    /**
+     * @var ChildGroupTypesProvider
+     */
+    protected $provider;
+
+    /**
+     * @before
+     */
+    public function setUpService()
+    {
+        $this->groupService = \Mockery::mock(GroupServiceInterface::class);
+        $this->provider = new ChildGroupTypesProvider($this->groupService);
+        $event = new Event();
+        $entity = new Entity(new GroupEntity());
+        $event->setParam('entity', $entity);
+        $this->provider->setEvent($event);
+    }
+
+    /**
+     * @test
+     */
+    public function testItShouldReturnChildGroupTypes()
+    {
+        $types = ['class'];
+        $this->groupService->shouldReceive('fetchChildTypes')
+            ->andReturn(['class'])
+            ->once();
+        $this->assertEquals($types, $this->provider->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function testItShouldReturnEmptyArrayIfEntityIsNonGroup()
+    {
+        $event = new Event();
+        $event->setParam('entity', new Entity(new UserEntity()));
+        $this->provider->setEvent($event);
+        $types = [];
+        $this->groupService->shouldReceive('fetchChildTypes')
+            ->never();
+        $this->assertEquals($types, $this->provider->getValue());
+    }
+}
---
+++ module/Org/test/OrgTest/Rule/Provider/OrgGroupTypesProviderTest.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace OrgTest\Rule\Provider;
+
+use Api\V1\Rest\Org\OrgEntity;
+use Api\V1\Rest\User\UserEntity;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Org\Rule\Provider\OrgGroupTypesProvider;
+use Org\Service\OrganizationServiceInterface;
+use Zend\EventManager\Event;
+use ZF\Hal\Entity;
+
+/**
+ * Class OrgGroupTypesProviderTest
+ * @package OrgTest\Rule\Provider
+ */
+class OrgGroupTypesProviderTest extends \PHPUnit_Framework_TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var \Mockery\MockInterface | OrganizationServiceInterface
+     */
+    protected $orgService;
+
+    /**
+     * @var OrgGroupTypesProvider
+     */
+    protected $provider;
+
+    /**
+     * @before
+     */
+    public function setUpService()
+    {
+        $this->orgService = \Mockery::mock(OrganizationServiceInterface::class);
+        $this->provider = new OrgGroupTypesProvider($this->orgService);
+        $event = new Event();
+        $entity = new Entity(new OrgEntity());
+        $event->setParam('entity', $entity);
+        $this->provider->setEvent($event);
+    }
+
+    /**
+     * @test
+     */
+    public function testItShouldReturnGroupTypesForOrg()
+    {
+        $types = ['class'];
+        $this->orgService->shouldReceive('fetchGroupTypes')
+            ->with($this->provider->getEvent()->getParam('entity')->getEntity())
+            ->andReturn(['class'])
+            ->once();
+        $this->assertEquals($types, $this->provider->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function testItShouldReturnEmptyArrayIfEntityIsNonOrg()
+    {
+        $event = new Event();
+        $event->setParam('entity', new Entity(new UserEntity()));
+        $this->provider->setEvent($event);
+        $types = [];
+        $this->orgService->shouldReceive('fetchGroupTypes')
+            ->andReturn([])
+            ->never();
+        $this->assertEquals($types, $this->provider->getValue());
+    }
+}
