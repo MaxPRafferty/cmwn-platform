c27e7fdf55fc8ccda6d282c5970f8f09ab836c4b
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2017-03-22 10:29:10 -0400

    Adding missing config for game rules

--- module/Game/config/module.config.php
+++ module/Game/config/module.config.php
@@ -3,12 +3,12 @@ return [
     \Zend\ServiceManager\AbstractFactory\ConfigAbstractFactory::class => [
         \Game\Service\UserGameService::class => ['Table/UserGames'],
     ],
-    'service_manager' => [
+    'service_manager'                                                 => [
         'aliases'    => [
             'Game\Service'                                => \Game\Service\GameService::class,
             \Game\Service\GameServiceInterface::class     => \Game\Service\GameService::class,
             \Game\Service\SaveGameServiceInterface::class => \Game\Service\SaveGameService::class,
-            \Game\Service\UserGameServiceInterface::class => \Game\Service\UserGameService::class
+            \Game\Service\UserGameServiceInterface::class => \Game\Service\UserGameService::class,
         ],
         'factories'  => [
             \Game\Delegator\SaveGameDelegatorFactory::class => \Zend\ServiceManager\Factory\InvokableFactory::class,
@@ -20,12 +20,34 @@ return [
             \Game\Service\SaveGameService::class => [
                 \Game\Delegator\SaveGameDelegatorFactory::class,
             ],
-            \Game\Service\GameService::class => [
+            \Game\Service\GameService::class     => [
                 \Game\Delegator\GameDelegatorFactory::class,
             ],
             \Game\Service\UserGameService::class => [
                 \Game\Delegator\UserGameDelegatorFactory::class,
             ],
+        ],
+    ],
+
+    'rules' => [
+        'factories' => [
+            \Game\Rule\Rule\UserCanPlayGame::class => \Rule\Rule\Service\BuildRuleFactory::class,
+            \Game\Rule\Rule\GameExists::class      => \Rule\Rule\Service\BuildRuleFactory::class,
+        ],
+
+        'shared' => [
+            \Game\Rule\Rule\UserCanPlayGame::class => false,
+            \Game\Rule\Rule\GameExists::class      => false,
+        ],
+    ],
+
+    'actions' => [
+        'factories' => [
+            \Game\Rule\Action\AddGameToUserAction::class => \Rule\Action\Service\BuildActionFactory::class,
+        ],
+
+        'shared' => [
+            \Game\Rule\Action\AddGameToUserAction::class => false,
         ],
     ],
 ];
